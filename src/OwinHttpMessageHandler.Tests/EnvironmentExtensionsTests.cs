068e3526dbca40dab32f34ccfd8db7de96a3ecef
Damian Hickey <dhickey@gmail.com>
2013-01-10 13:24:46 +0000

    Separate out assertions

--- src/OwinHttpMessageHandler.Tests/EnvironmentExtensionsTests.cs
+++ src/OwinHttpMessageHandler.Tests/EnvironmentExtensionsTests.cs
@@ -5,32 +5,96 @@
     using System.Threading;
     using OwinHttpMessageHander;
     using Xunit;
+    // ReSharper disable InconsistentNaming
 
     public class EnvironmentExtensionsTests
     {
-        [Fact]
-        public void Can_convert_request_to_environment()
+        private readonly IDictionary<string, object> _sut;
+
+        public EnvironmentExtensionsTests()
         {
             var request = new HttpRequestMessage(HttpMethod.Get, "https://example.com:8080/path?x=y")
-                          {
-                              Content = new StringContent("foo")
-                          };
-            IDictionary<string, object> env = request.ToEnvironment(new CancellationToken());
-
-            Assert.NotNull(env[Constants.VersionKey]);
-            Assert.NotNull(env[Constants.CallCancelledKey]);
-            Assert.Equal("127.0.0.1", env.Get<string>(Constants.ServerRemoteIpAddressKey));
-            Assert.Equal("1024", env.Get<string>(Constants.ServerRemotePortKey));
-            Assert.Equal("127.0.0.1", env.Get<string>(Constants.ServerLocalIpAddressKey));
-            Assert.Equal("8080", env.Get<string>(Constants.ServerLocalPortKey));
-            Assert.NotNull(env.Get<IList<IDictionary<string, object>>>(Constants.ServerCapabilities));
-            Assert.Equal(true, env.Get<bool>(Constants.ServerIsLocalKey));
-            Assert.Equal("GET", env.Get<string>(Constants.RequestMethodKey));
-            Assert.Equal("https", env.Get<string>(Constants.RequestSchemeKey));
-            Assert.Equal("/path", env.Get<string>(Constants.RequestPathKey));
-            Assert.Equal("x=y", env.Get<string>(Constants.RequestQueryStringKey));
+            {
+                Content = new StringContent("foo")
+            };
+            _sut = request.ToEnvironment(new CancellationToken());
         }
+
+        [Fact]
+        public void Should_have_Version()
+        {
+            Assert.NotNull(_sut[Constants.VersionKey]);
+        }
+
+        [Fact]
+        public void Should_have_CallCanceled()
+        {
+            Assert.NotNull(_sut[Constants.CallCancelledKey]);
+        }
+
+        [Fact]
+        public void Should_have_ServerRemoteIpAddress()
+        {
+            Assert.Equal("127.0.0.1", _sut.Get<string>(Constants.ServerRemoteIpAddressKey));
+        }
+
+        [Fact]
+        public void Should_have_ServerRemotePort()
+        {
+            Assert.Equal("1024", _sut.Get<string>(Constants.ServerRemotePortKey));
+        }
+
+        [Fact]
+        public void Should_have_ServerLocalIpAddress()
+        {
+            Assert.Equal("127.0.0.1", _sut.Get<string>(Constants.ServerLocalIpAddressKey));
+        }
+
+        [Fact]
+        public void Should_have_ServerLocalPort()
+        {
+            Assert.Equal("8080", _sut.Get<string>(Constants.ServerLocalPortKey));
+        }
+
+        [Fact]
+        public void Should_have_empty_server_capabilities()
+        {
+            Assert.NotNull(_sut.Get<IList<IDictionary<string, object>>>(Constants.ServerCapabilities));
+            Assert.Empty(_sut.Get<IList<IDictionary<string, object>>>(Constants.ServerCapabilities));
+        }
+
+        [Fact]
+        public void Should_have_IsLocal_true()
+        {
+            Assert.Equal(true, _sut.Get<bool>(Constants.ServerIsLocalKey));
+        }
+
+        [Fact]
+        public void Should_have_RequestMethod()
+        {
+            Assert.Equal("GET", _sut.Get<string>(Constants.RequestMethodKey));
+        }
+
+        [Fact]
+        public void Should_have_RequestPath()
+        {
+            Assert.Equal("/path", _sut.Get<string>(Constants.RequestPathKey));
+        }
+
+        [Fact]
+        public void Should_have_RequestScheme()
+        {
+            Assert.Equal("https", _sut.Get<string>(Constants.RequestSchemeKey));
+        }
+
+        [Fact]
+        public void Should_have_RequestQuery()
+        {
+            Assert.Equal("x=y", _sut.Get<string>(Constants.RequestQueryStringKey));
+        }
+      
     }
+    // ReSharper restore InconsistentNaming
 
     public static class DictionaryExtensions
     {
