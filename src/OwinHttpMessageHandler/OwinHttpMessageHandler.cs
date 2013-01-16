907331d915bf2f1dfe4be86951fb5eda4141a08d
Damian Hickey <dhickey@gmail.com>
2013-01-16 11:11:07 +0000

    Remove some unused constants

--- src/OwinHttpMessageHandler/OwinHttpMessageHandler.cs
+++ src/OwinHttpMessageHandler/OwinHttpMessageHandler.cs
@@ -124,9 +124,6 @@
             public const string ResponseHeadersKey = "owin.ResponseHeaders";
             public const string ResponseBodyKey = "owin.ResponseBody";
 
-            public const string ClientCertifiateKey = "ssl.ClientCertificate";
-            public const string LoadClientCertAsyncKey = "ssl.LoadClientCertAsync";
-
             public const string ServerRemoteIpAddressKey = "server.RemoteIpAddress";
             public const string ServerRemotePortKey = "server.RemotePort";
             public const string ServerLocalIpAddressKey = "server.LocalIpAddress";
@@ -136,18 +133,12 @@
             public const string ServerUserKey = "server.User";
             public const string ServerCapabilities = "server.Capabilities";
 
-            public const string WebSocketVersionKey = "websocket.Version";
-            public const string WebSocketVersion = "1.0";
-            public const string WebSocketAcceptKey = "websocket.Accept";
-            public const string WebSocketSubProtocolKey = "websocket.SubProtocol";
-
             public const string HostHeader = "Host";
             public const string WwwAuthenticateHeader = "WWW-Authenticate";
             public const string ContentLengthHeader = "Content-Length";
             public const string TransferEncodingHeader = "Transfer-Encoding";
             public const string KeepAliveHeader = "Keep-Alive";
             public const string ConnectionHeader = "Connection";
-            public const string SecWebSocketProtocol = "Sec-WebSocket-Protocol";
         }
     }
 }
