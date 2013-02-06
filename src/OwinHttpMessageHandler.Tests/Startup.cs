95154392b91a41dcca52431d346ecd766dc8df9b
Damian Hickey <dhickey@gmail.com>
2013-02-06 11:20:45 +0000

    Remove Gate and owin extensions, using Owin.Extension package instead

--- src/OwinHttpMessageHandler.Tests/Startup.cs
+++ src/OwinHttpMessageHandler.Tests/Startup.cs
@@ -2,27 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
-    using Gate;
     using Owin;
+    using Owin.Types;
 
     public class Startup
-	{
-        private readonly Dictionary<string, Action<Request, Response>> _responders = new Dictionary<string, Action<Request, Response>>();
+    {
+        private readonly Dictionary<string, Action<OwinRequest, OwinResponse>> _responders =
+            new Dictionary<string, Action<OwinRequest, OwinResponse>>();
 
-	    public Startup()
-	    {
-	        _responders.Add("/OK", (request, response) => response.StatusCode = 200);
-	        _responders.Add("/NotFound", (request, response) => response.StatusCode = 404);
-	        _responders.Add("/greeting", (request, response) =>
-	                                     {
-	                                         IDictionary<string, string> form = request.ReadForm();
-                                             response.Write("Hello "+ form["Name"]);
-	                                     });
-	    }
+        public Startup()
+        {
+            _responders.Add("/OK", (request, response) => response.StatusCode = 200);
+            _responders.Add("/NotFound", (request, response) => response.StatusCode = 404);
+            _responders.Add("/greeting", (request, response) =>
+                                         {
+                                             IDictionary<string, string> form = request.ReadForm();
+                                             response.Write("Hello " + form["Name"]);
+                                         });
+        }
 
-		public void Configuration(IAppBuilder builder)
-		{
-			builder.UseGate((request, response) => _responders[request.Path](request, response));
-		}
-	}
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseFilter(request =>  _responders[request.Path](request, new OwinResponse(request)));
+        }
+    }
 }
