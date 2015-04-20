39b3ca7144d8aa38dcc364d1c9bd00498ceb6602
Damian Hickey <dhickey@gmail.com>
2015-04-20 18:47:18 +0200

    Update LibOwin

--- src/OwinHttpMessageHandler/OwinHttpMessageHandler.cs
+++ src/OwinHttpMessageHandler/OwinHttpMessageHandler.cs
@@ -5,7 +5,7 @@
     using System.IO;
     using System.Linq;
     using System.Net;
-    using System.Net.Http.Owin;
+    using System.Net.Http.LibOwin;
     using System.Threading;
     using System.Threading.Tasks;
 
