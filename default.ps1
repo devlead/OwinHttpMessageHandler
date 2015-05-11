d7ddd68b88aad5b86a009939aac8a652d0da5f45
Damian Hickey <dhickey@gmail.com>
2015-05-11 07:51:56 +0200

    Don't need to clean the artifacts. Also tabs-> spaces.

--- default.ps1
+++ default.ps1
@@ -1,93 +1,92 @@
 properties {
-	$projectName = "OwinHttpMessageHandler"
-	$buildNumber = 0
-	$rootDir  = Resolve-Path .\
-	$buildOutputDir = "$rootDir\build"
-	$reportsDir = "$buildOutputDir\reports"
-	$srcDir = "$rootDir\src"
-	$solutionFilePath = "$srcDir\$projectName.sln"
-	$assemblyInfoFilePath = "$srcDir\SharedAssemblyInfo.cs"
-	$nugetPath = "$srcDir\.nuget\nuget.exe"
+    $projectName = "OwinHttpMessageHandler"
+    $buildNumber = 0
+    $rootDir  = Resolve-Path .\
+    $buildOutputDir = "$rootDir\build"
+    $reportsDir = "$buildOutputDir\reports"
+    $srcDir = "$rootDir\src"
+    $solutionFilePath = "$srcDir\$projectName.sln"
+    $assemblyInfoFilePath = "$srcDir\SharedAssemblyInfo.cs"
+    $nugetPath = "$srcDir\.nuget\nuget.exe"
 }
 
 task default -depends UpdateVersion, RunTests, CreateNuGetPackages
 
 task Clean {
-	Remove-Item $buildOutputDir -Force -Recurse -ErrorAction SilentlyContinue
-	exec { msbuild /nologo /verbosity:quiet $solutionFilePath /t:Clean }
+    Remove-Item $buildOutputDir -Force -Recurse -ErrorAction SilentlyContinue
+    exec { msbuild /nologo /verbosity:quiet $solutionFilePath /t:Clean }
 }
 
 task UpdateVersion {
-	$version = Get-Version $assemblyInfoFilePath
-	$oldVersion = New-Object Version $version
-	$newVersion = New-Object Version ($oldVersion.Major, $oldVersion.Minor, $oldVersion.Build, $buildNumber)
-	Update-Version $newVersion $assemblyInfoFilePath
+    $version = Get-Version $assemblyInfoFilePath
+    $oldVersion = New-Object Version $version
+    $newVersion = New-Object Version ($oldVersion.Major, $oldVersion.Minor, $oldVersion.Build, $buildNumber)
+    Update-Version $newVersion $assemblyInfoFilePath
 }
 
 task Compile -depends Clean {
-	exec { msbuild /nologo /verbosity:quiet $solutionFilePath /p:Configuration=Release }
+    exec { msbuild /nologo /verbosity:quiet $solutionFilePath /p:Configuration=Release }
 }
 
 task RunTests -depends Compile {
-	$xunitRunner = "$srcDir\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
-	gci . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
-		$project = $_.BaseName
-		if(!(Test-Path $reportsDir\xUnit\$project)){
-			New-Item $reportsDir\xUnit\$project -Type Directory
-		}
+    $xunitRunner = "$srcDir\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+    gci . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
+        $project = $_.BaseName
+        if(!(Test-Path $reportsDir\xUnit\$project)){
+            New-Item $reportsDir\xUnit\$project -Type Directory
+        }
         .$xunitRunner "$srcDir\$project\bin\Release\$project.dll" /html "$reportsDir\xUnit\$project\index.html"
     }
 }
 
 task BuildDnx {
-	Remove-Item "$srcDir\artifacts" -Force -Recurse -ErrorAction SilentlyContinue
-	&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
-	dnvm upgrade
-	pushd
-	cd .\src
-	dnu restore
-	dnu build .\OwinHttpMessageHandler --configuration Release
-	dnu pack .\OwinHttpMessageHandler --configuration Release --out $buildOutputDir
-	dnu build .\OwinHttpMessageHandler.Tests --configuration Release
-	dnx .\OwinHttpMessageHandler.Tests test
-	popd
+    &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
+    dnvm upgrade
+    pushd
+    cd .\src
+    dnu restore
+    dnu build .\OwinHttpMessageHandler --configuration Release
+    dnu pack .\OwinHttpMessageHandler --configuration Release --out $buildOutputDir
+    dnu build .\OwinHttpMessageHandler.Tests --configuration Release
+    dnx .\OwinHttpMessageHandler.Tests test
+    popd
 }
 
 task CreateNuGetPackages -depends Compile {
-	$versionString = Get-Version $assemblyInfoFilePath
-	$version = New-Object Version $versionString
-	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $buildNumber.ToString().PadLeft(5,'0')
-	gci $srcDir -Recurse -Include *.nuspec | % {
-		exec { .$srcDir\.nuget\nuget.exe pack $_ -o $buildOutputDir -version $packageVersion }
-	}
+    $versionString = Get-Version $assemblyInfoFilePath
+    $version = New-Object Version $versionString
+    $packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $buildNumber.ToString().PadLeft(5,'0')
+    gci $srcDir -Recurse -Include *.nuspec | % {
+        exec { .$srcDir\.nuget\nuget.exe pack $_ -o $buildOutputDir -version $packageVersion }
+    }
 }
 
 function Get-Version
 {
-	param
-	(
-		[string]$assemblyInfoFilePath
-	)
-	Write-Host "path $assemblyInfoFilePath"
-	$pattern = '(?<=^\[assembly\: AssemblyVersion\(\")(?<versionString>\d+\.\d+\.\d+\.\d+)(?=\"\))'
-	$assmblyInfoContent = Get-Content $assemblyInfoFilePath
-	return $assmblyInfoContent | Select-String -Pattern $pattern | Select -expand Matches |% {$_.Groups['versionString'].Value}
+    param
+    (
+        [string]$assemblyInfoFilePath
+    )
+    Write-Host "path $assemblyInfoFilePath"
+    $pattern = '(?<=^\[assembly\: AssemblyVersion\(\")(?<versionString>\d+\.\d+\.\d+\.\d+)(?=\"\))'
+    $assmblyInfoContent = Get-Content $assemblyInfoFilePath
+    return $assmblyInfoContent | Select-String -Pattern $pattern | Select -expand Matches |% {$_.Groups['versionString'].Value}
 }
 
 function Update-Version
 {
-	param
+    param
     (
-		[string]$version,
-		[string]$assemblyInfoFilePath
-	)
+        [string]$version,
+        [string]$assemblyInfoFilePath
+    )
 
-	$newVersion = 'AssemblyVersion("' + $version + '")';
-	$newFileVersion = 'AssemblyFileVersion("' + $version + '")';
-	$tmpFile = $assemblyInfoFilePath + ".tmp"
+    $newVersion = 'AssemblyVersion("' + $version + '")';
+    $newFileVersion = 'AssemblyFileVersion("' + $version + '")';
+    $tmpFile = $assemblyInfoFilePath + ".tmp"
 
-	Get-Content $assemblyInfoFilePath |
-		%{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newFileVersion }  | Out-File -Encoding UTF8 $tmpFile
+    Get-Content $assemblyInfoFilePath |
+        %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newFileVersion }  | Out-File -Encoding UTF8 $tmpFile
 
-	Move-Item $tmpFile $assemblyInfoFilePath -force
+    Move-Item $tmpFile $assemblyInfoFilePath -force
 }
